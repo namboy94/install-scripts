21e1095d3d4807c0788ab3b2b8baa08291e2b6dd
Hermann Krumrey <hermann@krumreyh.com>
2018-03-27 10:33:20 +0200

    Add reddit blocking script and other minor additions

--- linux/general/generate-bashrc.sh
+++ linux/general/generate-bashrc.sh
@@ -19,9 +19,6 @@
 mkdir -p ~/.local/bin
 mkdir -p ~/Temp
 
-wget https://raw.githubusercontent.com/will8211/unimatrix/369256134093b62910b31ae505765301cff46d38/unimatrix.py -O ~/.local/bin/unimatrix
-chmod 755 ~/.local/bin/unimatrix
-
 echo '[[ $- != *i* ]] && return
 export PS1="[\[$(tput sgr0)\]\[\033[38;5;10m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;27m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\]:\[$(tput sgr0)\]\[\033[38;5;11m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]]\[$(tput sgr0)\]"
 alias ls="ls --color=auto -lh"
@@ -31,7 +28,8 @@ VISUAL=/usr/bin/nano
 PATH=$PATH:~/.local/bin
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 alias mount-temp="sshfs -o idmap=user hermann@192.168.1.2:/mnt/Main/Temp ~/Temp"
-~/.local/bin/unimatrix -t 2 -n -s 96 -l o
+alias backup-home="rsync -av ~/ 192.168.1.2:/mnt/Main/Backups/Linux/$(hostname -f)"
+alias youtube-mp3="youtube-dl --extract-audio --audio-format mp3"
 ' > ~/.bashrc
 
 echo '
